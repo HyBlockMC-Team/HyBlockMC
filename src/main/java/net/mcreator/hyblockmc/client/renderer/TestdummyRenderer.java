df1dda810e64d5a9e1775f34655343aff63c746d
callemkay <callemkay@gmail.com>
2023-10-25 01:55:53 +0200

    removed the redundant player model

--- src/main/java/net/mcreator/hyblockmc/client/renderer/TestdummyRenderer.java
+++ src/main/java/net/mcreator/hyblockmc/client/renderer/TestdummyRenderer.java
@@ -2,15 +2,18 @@
 package net.mcreator.hyblockmc.client.renderer;
 
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.client.renderer.entity.layers.HumanoidArmorLayer;
+import net.minecraft.client.renderer.entity.HumanoidMobRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.model.geom.ModelLayers;
+import net.minecraft.client.model.HumanoidModel;
 
 import net.mcreator.hyblockmc.entity.TestdummyEntity;
-import net.mcreator.hyblockmc.client.model.Modelcustom_model;
 
-public class TestdummyRenderer extends MobRenderer<TestdummyEntity, Modelcustom_model<TestdummyEntity>> {
+public class TestdummyRenderer extends HumanoidMobRenderer<TestdummyEntity, HumanoidModel<TestdummyEntity>> {
 	public TestdummyRenderer(EntityRendererProvider.Context context) {
-		super(context, new Modelcustom_model(context.bakeLayer(Modelcustom_model.LAYER_LOCATION)), 0.5f);
+		super(context, new HumanoidModel(context.bakeLayer(ModelLayers.PLAYER)), 0.5f);
+		this.addLayer(new HumanoidArmorLayer(this, new HumanoidModel(context.bakeLayer(ModelLayers.PLAYER_INNER_ARMOR)), new HumanoidModel(context.bakeLayer(ModelLayers.PLAYER_OUTER_ARMOR)), context.getModelManager()));
 	}
 
 	@Override
--- src/main/java/net/mcreator/hyblockmc/entity/TestdummyEntity.java
+++ src/main/java/net/mcreator/hyblockmc/entity/TestdummyEntity.java
@@ -75,6 +75,11 @@ public class TestdummyEntity extends PathfinderMob {
 	}
 
 	@Override
+	public double getMyRidingOffset() {
+		return -0.35D;
+	}
+
+	@Override
 	public SoundEvent getHurtSound(DamageSource ds) {
 		return ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.generic.hurt"));
 	}
