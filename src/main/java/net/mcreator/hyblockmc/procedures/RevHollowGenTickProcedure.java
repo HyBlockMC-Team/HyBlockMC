7b204ebf687c3ae9dcae07e5a69160663a499eab
callemkay <callemkay@gmail.com>
2023-10-26 14:51:48 +0200

    tested with changing around some stuff

--- src/main/java/net/mcreator/hyblockmc/procedures/RevHollowGenTickProcedure.java
+++ src/main/java/net/mcreator/hyblockmc/procedures/RevHollowGenTickProcedure.java
@@ -7,7 +7,7 @@ import net.mcreator.hyblockmc.init.HyblockModBlocks;
 
 public class RevHollowGenTickProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
-		world.setBlock(BlockPos.containing(x, y - 18, z), HyblockModBlocks.REV_HOLLOW_GEN.get().defaultBlockState(), 3);
-		world.scheduleTick(BlockPos.containing(x, y - 18, z), world.getBlockState(BlockPos.containing(x, y - 18, z)).getBlock(), 20);
+		world.setBlock(BlockPos.containing(x, y - 15, z), HyblockModBlocks.REV_HOLLOW_GEN.get().defaultBlockState(), 3);
+		world.scheduleTick(BlockPos.containing(x, y - 15, z), world.getBlockState(BlockPos.containing(x, y - 15, z)).getBlock(), 20);
 	}
 }
